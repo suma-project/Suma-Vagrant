3da72002edac5fdfbdb1c0371479d84616cbdd76
Bret Davidson <bret_davidson@ncsu.edu>
2015-03-20 15:35:40 -0400

    add provision-dev task

--- guest_ansible.sh
+++ guest_ansible.sh
@@ -9,6 +9,10 @@ elif [ "$1" == "npm-install" ]
 then
   echo "Running npm-install on guest..."
   vagrant ssh -- 'cd /vagrant && /usr/bin/ansible-playbook /vagrant/ansible_tasks/development.yml --tags=npm-install'
+elif [ "$1" == "provision-dev" ]
+then
+  echo "Provisioning development environment on guest (this may take awhile)..."
+  vagrant ssh -- 'cd /vagrant && /usr/bin/ansible-playbook /vagrant/ansible_tasks/development.yml'
 elif [ "$1" == "grunt-test" ]
 then
   echo "Running grunt tests on guest..."
@@ -27,12 +31,14 @@ then
   echo "Running grunt lint on guest..."
   vagrant ssh -- 'cd /vagrant/suma/analysis && /usr/local/bin/grunt lint'
 else
-  echo -e "\n\nUnknown or missing command, options are:"
-  echo "  bower-install - Execute bower install for analysis tools"
-  echo "  grunt         - Execute grunt lint, test, and build for analysis tools"
-  echo "  grunt-build   - Execute grunt build task for analysis tools"
-  echo "  grunt-lint    - Execute grunt lint for analysis tools"
-  echo "  grunt-test    - Execute grunt tests for analysis tools"
-  echo "  grunt-watch   - Execute grunt watch for analysis tools"
-  echo "  npm-install   - Execute npm install for analysis tools"
+  echo -e "\nUnknown or missing command, options are:\n"
+  echo -e "\tbower-install - Execute bower install for analysis tools"
+  echo -e "\tgrunt         - Execute grunt lint, test, and build for analysis tools"
+  echo -e "\tgrunt-build   - Execute grunt build task for analysis tools"
+  echo -e "\tgrunt-lint    - Execute grunt lint for analysis tools"
+  echo -e "\tgrunt-test    - Execute grunt tests for analysis tools"
+  echo -e "\tgrunt-watch   - Execute grunt watch for analysis tools"
+  echo -e "\tnpm-install   - Execute npm install for analysis tools"
+  echo -e "\tprovision-dev - Provision the development environment on the guest"
+  echo -e "\n"
 fi
