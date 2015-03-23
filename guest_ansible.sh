1e7ed87b14f4763b9cbe46c8f33895c03e418df0
Bret Davidson <bret_davidson@ncsu.edu>
2015-03-23 11:03:28 -0400

    add clean tasks

--- guest_ansible.sh
+++ guest_ansible.sh
@@ -5,10 +5,18 @@ if [ "$1" == "bower-install" ]
 then
   echo "Running bower install on guest..."
   vagrant ssh -- 'cd /vagrant/suma/analysis && /usr/local/bin/bower install'
+elif [ "$1" == "bower-clean" ]
+then
+  echo "Running bower-clean on guest..."
+  vagrant ssh -- 'cd /vagrant/suma/analysis/src && rm -r bower_components'
 elif [ "$1" == "npm-install" ]
 then
   echo "Running npm-install on guest..."
   vagrant ssh -- 'cd /vagrant && /usr/bin/ansible-playbook /vagrant/ansible_tasks/development.yml --tags=npm-install'
+elif [ "$1" == "npm-clean" ]
+then
+  echo "Running npm-clean on guest..."
+  vagrant ssh -- 'cd /vagrant/suma/analysis && rm -r node_modules'
 elif [ "$1" == "provision-dev" ]
 then
   echo "Provisioning development environment on guest (this may take awhile)..."
@@ -32,12 +40,14 @@ then
   vagrant ssh -- 'cd /vagrant/suma/analysis && /usr/local/bin/grunt lint'
 else
   echo -e "\nUnknown or missing command, options are:\n"
+  echo -e "\tbower-clean   - Clean bower_components directory"
   echo -e "\tbower-install - Execute bower install for analysis tools"
   echo -e "\tgrunt         - Execute grunt lint, test, and build for analysis tools"
   echo -e "\tgrunt-build   - Execute grunt build task for analysis tools"
   echo -e "\tgrunt-lint    - Execute grunt lint for analysis tools"
   echo -e "\tgrunt-test    - Execute grunt tests for analysis tools"
   echo -e "\tgrunt-watch   - Execute grunt watch for analysis tools"
+  echo -e "\tnpm-clean     - Clean node_modules directory"
   echo -e "\tnpm-install   - Execute npm install for analysis tools"
   echo -e "\tprovision-dev - Provision the development environment on the guest"
   echo -e "\n"
