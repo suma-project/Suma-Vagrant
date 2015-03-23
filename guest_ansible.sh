aff22de651bea687b5dfc041418091908c1bfaf4
Bret Davidson <bret_davidson@ncsu.edu>
2015-03-23 11:51:34 -0400

    expose config tasks

--- guest_ansible.sh
+++ guest_ansible.sh
@@ -38,17 +38,27 @@ elif [ "$1" == "grunt-lint" ]
 then
   echo "Running grunt lint on guest..."
   vagrant ssh -- 'cd /vagrant/suma/analysis && /usr/local/bin/grunt lint'
+elif [ "$1" == "apache-configs" ]
+then
+  echo "Running apache-configs on guest..."
+  vagrant ssh -- 'cd /vagrant && /usr/bin/ansible-playbook /vagrant/ansible_tasks/copy_apache_configs.yml'
+elif [ "$1" == "suma-configs" ]
+then
+  echo "Running suma-configs on guest..."
+  vagrant ssh -- 'cd /vagrant && /usr/bin/ansible-playbook /vagrant/ansible_tasks/copy_suma_configs.yml'
 else
   echo -e "\nUnknown or missing command, options are:\n"
-  echo -e "\tbower-clean   - Clean bower_components directory"
-  echo -e "\tbower-install - Execute bower install for analysis tools"
-  echo -e "\tgrunt         - Execute grunt lint, test, and build for analysis tools"
-  echo -e "\tgrunt-build   - Execute grunt build task for analysis tools"
-  echo -e "\tgrunt-lint    - Execute grunt lint for analysis tools"
-  echo -e "\tgrunt-test    - Execute grunt tests for analysis tools"
-  echo -e "\tgrunt-watch   - Execute grunt watch for analysis tools"
-  echo -e "\tnpm-clean     - Clean node_modules directory"
-  echo -e "\tnpm-install   - Execute npm install for analysis tools"
-  echo -e "\tprovision-dev - Provision the development environment on the guest"
+  echo -e "\tapache-configs - Copy apache configs"
+  echo -e "\tbower-clean    - Clean bower_components directory"
+  echo -e "\tbower-install  - Execute bower install for analysis tools"
+  echo -e "\tsuma-configs   - Copy suma configs"
+  echo -e "\tgrunt          - Execute grunt lint, test, and build for analysis tools"
+  echo -e "\tgrunt-build    - Execute grunt build task for analysis tools"
+  echo -e "\tgrunt-lint     - Execute grunt lint for analysis tools"
+  echo -e "\tgrunt-test     - Execute grunt tests for analysis tools"
+  echo -e "\tgrunt-watch    - Execute grunt watch for analysis tools"
+  echo -e "\tnpm-clean      - Clean node_modules directory"
+  echo -e "\tnpm-install    - Execute npm install for analysis tools"
+  echo -e "\tprovision-dev  - Provision the development environment on the guest"
   echo -e "\n"
 fi
