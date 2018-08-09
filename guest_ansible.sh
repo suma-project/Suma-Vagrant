bc48ba33933d66d457a3b5c801dbba8bea4add14
Bret Davidson <bretdavidson@gmail.com>
2018-08-09 09:52:39 -0400

    fix guest task paths

--- guest_ansible.sh
+++ guest_ansible.sh
@@ -3,7 +3,7 @@
 if [ "$1" == "bower-install" ]
 then
   echo "Running bower install on guest..."
-  vagrant ssh -- 'cd /vagrant/suma/analysis && /usr/local/bin/bower install'
+  vagrant ssh -- 'cd /vagrant/suma/analysis && /usr/bin/bower install'
 elif [ "$1" == "bower-clean" ]
 then
   echo "Running bower-clean on guest..."
@@ -11,15 +11,15 @@ then
 elif [ "$1" == "npm-install" ]
 then
   echo "Running npm-install on guest..."
-  vagrant ssh -- 'cd /vagrant/ansible && /usr/bin/ansible-playbook /vagrant/ansible/development.yml --tags=npm-install --inventory=/vagrant/ansible/inventories/development.ini'
+  vagrant ssh -- 'cd /vagrant/ansible && /usr/bin/ansible-playbook /vagrant/ansible/demo.yml --tags=npm-install --inventory=/vagrant/ansible/inventories/development.ini'
 elif [ "$1" == "npm-clean" ]
 then
   echo "Running npm-clean on guest..."
-  vagrant ssh -- 'cd /vagrant/suma/analysis && rm -r node_modules'
+  vagrant ssh -- 'cd /vagrant/suma/analysis && rm -r node_modules/*'
 elif [ "$1" == "grunt" ]
 then
   echo "Running default grunt tasks on guest..."
-  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/local/bin/grunt'
+  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/bin/grunt'
 elif [ "$1" == "grunt-test" ]
 then
   echo "Running grunt tests on guest..."
@@ -27,16 +27,16 @@ then
 elif [ "$1" == "grunt-build" ]
 then
   echo "Running grunt build on guest..."
-  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/local/bin/grunt build'
+  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/bin/grunt build'
 elif [ "$1" == "grunt-watch" ]
 then
   echo "Running grunt watch on guest..."
   echo "NOTE: this task will continue to run"
-  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/local/bin/grunt watch'
+  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/bin/grunt watch'
 elif [ "$1" == "grunt-lint" ]
 then
   echo "Running grunt lint on guest..."
-  vagrant ssh -- 'cd /vagrant/suma/analysis && /usr/local/bin/grunt lint'
+  vagrant ssh -- 'cd /vagrant/suma/analysis && /usr/bin/grunt lint'
 else
   echo -e "\nUnknown or missing command, options are:\n"
   echo -e "\tbower-clean    - Clean bower_components directory"
