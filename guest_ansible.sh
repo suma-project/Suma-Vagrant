2233391add4535b7675fda5e4bcfa66a373de689
Bret Davidson <bretdavidson@gmail.com>
2018-08-09 09:22:15 -0400

    update development script

--- guest_ansible.sh
+++ guest_ansible.sh
@@ -16,45 +16,31 @@ elif [ "$1" == "npm-clean" ]
 then
   echo "Running npm-clean on guest..."
   vagrant ssh -- 'cd /vagrant/suma/analysis && rm -r node_modules'
-elif [ "$1" == "provision-dev" ]
-then
-  echo "Provisioning development environment on guest (this may take awhile)..."
-  vagrant ssh -- 'cd /vagrant/ansible && /usr/bin/ansible-playbook /vagrant/ansible/development.yml --inventory=/vagrant/ansible/inventories/development.ini'
 elif [ "$1" == "grunt" ]
 then
   echo "Running default grunt tasks on guest..."
-  vagrant ssh -- 'source /etc/profile.d/rvm.sh; cd /vagrant/suma/analysis && /usr/local/bin/grunt'
+  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/local/bin/grunt'
 elif [ "$1" == "grunt-test" ]
 then
   echo "Running grunt tests on guest..."
-  vagrant ssh -- 'source /etc/profile.d/rvm.sh; cd /vagrant/suma/analysis && /usr/local/bin/grunt test'
+  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/bin/grunt test'
 elif [ "$1" == "grunt-build" ]
 then
   echo "Running grunt build on guest..."
-  vagrant ssh -- 'source /etc/profile.d/rvm.sh; cd /vagrant/suma/analysis && /usr/local/bin/grunt build'
+  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/local/bin/grunt build'
 elif [ "$1" == "grunt-watch" ]
 then
   echo "Running grunt watch on guest..."
   echo "NOTE: this task will continue to run"
-  vagrant ssh -- 'source /etc/profile.d/rvm.sh; cd /vagrant/suma/analysis && /usr/local/bin/grunt watch'
+  vagrant ssh -- 'source /home/vagrant/.bash_profile; cd /vagrant/suma/analysis && /usr/local/bin/grunt watch'
 elif [ "$1" == "grunt-lint" ]
 then
   echo "Running grunt lint on guest..."
   vagrant ssh -- 'cd /vagrant/suma/analysis && /usr/local/bin/grunt lint'
-elif [ "$1" == "apache-configs" ]
-then
-  echo "Running apache-configs on guest..."
-  vagrant ssh -- 'cd /vagrant/ansible && /usr/bin/ansible-playbook /vagrant/ansible/copy_apache_configs.yml --inventory=/vagrant/ansible/inventories/development.ini'
-elif [ "$1" == "suma-configs" ]
-then
-  echo "Running suma-configs on guest..."
-  vagrant ssh -- 'cd /vagrant/ansible && /usr/bin/ansible-playbook /vagrant/ansible/copy_suma_configs.yml --inventory=/vagrant/ansible/inventories/development.ini'
 else
   echo -e "\nUnknown or missing command, options are:\n"
-  echo -e "\tapache-configs - Copy apache configs"
   echo -e "\tbower-clean    - Clean bower_components directory"
   echo -e "\tbower-install  - Execute bower install for analysis tools"
-  echo -e "\tsuma-configs   - Copy suma configs"
   echo -e "\tgrunt          - Execute grunt lint, test, and build for analysis tools"
   echo -e "\tgrunt-build    - Execute grunt build task for analysis tools"
   echo -e "\tgrunt-lint     - Execute grunt lint for analysis tools"
@@ -62,6 +48,5 @@ else
   echo -e "\tgrunt-watch    - Execute grunt watch for analysis tools"
   echo -e "\tnpm-clean      - Clean node_modules directory"
   echo -e "\tnpm-install    - Execute npm install for analysis tools"
-  echo -e "\tprovision-dev  - Provision the development environment on the guest"
   echo -e "\n"
 fi
